9c12b75edf4280b1b41414f522d800fa1a5f6e9e
sarathkumar2509 <smartsarath2509.ss@gmail.com>
2020-10-25 20:50:45 +0530

    Swipe to delete

--- app/src/main/java/com/example/todo/ui/ToDo/ToDoActivity.kt
+++ app/src/main/java/com/example/todo/ui/ToDo/ToDoActivity.kt
@@ -36,6 +36,28 @@ class ToDoActivity : AppCompatActivity() {
         })
 
 
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val item = adapter.differ.currentList[position]
+                viewModel.delete(item)
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(rvToDoitems)
+        }
 
         fabAdd.setOnClickListener{
             AddToDoItemBottomDialog(this,
@@ -45,6 +67,9 @@ class ToDoActivity : AppCompatActivity() {
                 }
             }).show(supportFragmentManager,"BottomSheetDialog")
         }
+
+
+
 
     }
 }
